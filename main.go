0dbee2707711391acdff40590cc738a54cd61d20
matsuhashi-tatsuya <matsuhashi-tatsuya@dmm.com>
2023-08-08 00:36:03 +0900

    ローカルサーバ立ち上げ

--- main.go
+++ main.go
@@ -1,38 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	baseURL := "https://azami-jp.com/live"
 
 	// Goquery
 	resp, err := fetch(baseURL)
-
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	infosByGoquery, err := parseByGoquery(resp)
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Println("Goquery")
+	fmt.Fprintln(w, "Goquery")
 	for _, item := range infosByGoquery {
-		fmt.Println(item)
-		// fmt.Println(info, info.URL) // TODO ランタイムのバグか？出力が異なる
+		fmt.Fprintln(w, item)
+		// fmt.Fprintln(w, info, info.URL) // TODO ランタイムのバグか？出力が異なる
 	}
 
 	// Colly
 	infosByColly, err := parseByColly(baseURL)
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Println("====================================")
-	fmt.Println("Colly")
+	fmt.Fprintln(w, "====================================")
+	fmt.Fprintln(w, "Colly")
 	for _, info := range infosByColly {
-		fmt.Println(info)
-		// fmt.Println(info, info.URL) // TODO ランタイムのバグか？出力が異なる
+		fmt.Fprintln(w, info)
+		// fmt.Fprintln(w, info, info.URL) // TODO ランタイムのバグか？出力が異なる
 	}
 }
